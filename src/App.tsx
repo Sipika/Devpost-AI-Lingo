c8b8650178e01c420c01edbd2465605b9b280dca
Saurabh <sipika.1rn11is048@gmail.com>
2026-07-18 18:28:03 +0200

    Fix NavBar import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavBar } from './components/NavBar.tsx';
+import { NavBar } from './components/NavBar';
 
 type Tab = 'game' | 'podcasts' | 'reading' | 'events';
 
